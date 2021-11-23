a6fb62b12018e162847e6de10d0d85c9c8526da0
vytautasbruzgis <93397096+vytautasbruzgis@users.noreply.github.com>
2021-11-23 19:05:21 +0200

    Static metodo pavyzys

---
+++ SampleConsole/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsole
+{
+    public class Car
+    {
+        public void StartEngine()
+        {
+            Console.WriteLine("Start the engine .... ");
+        }
+
+        public static void Static_StartEngine()
+        {
+            Console.WriteLine("Start the engine in static .... ");
+        }
+    }
+}
--- SampleConsole/Program.cs
+++ SampleConsole/Program.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using SampleConsole;
+using System;
 
 namespace SampleConsoleProjects
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            var car1 = new Car();
+            var car2 = new Car();
+
+            car1.StartEngine();
+            Car.Static_StartEngine();
+
+            var randomnumber = "5";
+            PrintInfo(randomnumber);
+        }
+
+        public static void PrintInfo(string info)
         {
             Console.WriteLine("Hello, World!");
         }
