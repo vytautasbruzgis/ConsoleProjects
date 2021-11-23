6d3f127a685b72e0827ad13c048c55e094193273
vytautasbruzgis <93397096+vytautasbruzgis@users.noreply.github.com>
2021-11-23 18:43:35 +0200

    Praktikuojamės commitinti

--- SampleConsole/Program.cs
+++ SampleConsole/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SampleConsoleProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+
